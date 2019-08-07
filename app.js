33b2e51bebe24b1b9f2436dc632bcfec7a914ed1
Pavel <p.v.hromov@gmail.com>
2019-08-07 12:52:09 -0400

    MongoDB and Mongoose

--- app.js
+++ app.js
@@ -1,27 +1,65 @@
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser"); 
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/two_wheels_trips");
+
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+//SCHEMA SETUP
+
+var tripsSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Trips = mongoose.model("Trips", tripsSchema);
+
+// Trips.create(
+// {
+// 	name: "Seven Lakes",
+// 	image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"
+
+
+// }, function(err, trips){
+// 	if(err){
+// 		console.log(err);
+// 	} else {
+// 		console.log("newly created trip: ");
+// 		console.log(trips);
+// 	}
+// });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var trips = [
-		{name: "Seven Lakes", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"},
-		{name: "Catskills", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"},
-		{name: "Catskills", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"},
-		{name: "Mount Washington", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"},
-		{name: "Mount Washington", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"},
-		{name: "Bushkills Falls", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"}
-];
+// var trips = [
+// 		{name: "Seven Lakes", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"},
+// 		{name: "Catskills", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"},
+// 		{name: "Catskills", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"},
+// 		{name: "Mount Washington", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"},
+// 		{name: "Mount Washington", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"},
+// 		{name: "Bushkills Falls", image: "https://climatecommunication.yale.edu/wp-content/uploads/2017/04/001-stone-circle-jpeg-768x350.jpg"}
+// ];
 
 app.get("/", function(req, res){
 	res.render("landing");
 });
 
 app.get("/trips", function(req, res){
+	// Get all trips from DB
+	Trips.find({}, function(err, allTrips){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("trips", {trips:allTrips});
+		}
+	});
 	
-
-	res.render("trips", {trips:trips});
 });
 
 app.post("/trips", function(req, res){
@@ -30,14 +68,21 @@ app.post("/trips", function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var newTrips = {name: name, image: image}
-	trips.push(newTrips);
-	// redirect back to trips page
-	res.redirect("/trips");
+	// Create a new trip and save to the DB
+	Trips.create(newTrips, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect("/trips");
+		}
+	});
 });
 
 app.get("/trips/new", function(req, res){
 	res.render("new.ejs");
 });
+
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
