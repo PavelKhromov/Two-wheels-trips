7f88bd85e416ef275c6c933787ee5cdba6d046fe
Pavel <p.v.hromov@gmail.com>
2019-11-17 22:31:51 -0500

    app

--- app.js
+++ app.js
@@ -18,27 +18,27 @@ var commentRoutes  = require("./routes/comments"),
     indexRoutes    = require("./routes/index") 
 
     //new code
-const http = require('http');
+// const http = require('http');
 
-process
-  .on('SIGTERM', shutdown('SIGTERM'))
-  .on('SIGINT', shutdown('SIGINT'))
-  .on('uncaughtException', shutdown('uncaughtException'));
+// process
+//   .on('SIGTERM', shutdown('SIGTERM'))
+//   .on('SIGINT', shutdown('SIGINT'))
+//   .on('uncaughtException', shutdown('uncaughtException'));
 
-setInterval(console.log.bind(console, 'tick'), 1000);
-http.createServer((req, res) => res.end('hi'))
-  .listen(process.env.PORT || 3000, () => console.log('Listening'));
+// setInterval(console.log.bind(console, 'tick'), 1000);
+// http.createServer((req, res) => res.end('hi'))
+//   .listen(process.env.PORT || 3000, () => console.log('Listening'));
 
-function shutdown(signal) {
-  return (err) => {
-    console.log(`${ signal }...`);
-    if (err) console.error(err.stack || err);
-    setTimeout(() => {
-      console.log('...waited 5s, exiting.');
-      process.exit(err ? 1 : 0);
-    }, 5000).unref();
-  };
-}
+// function shutdown(signal) {
+//   return (err) => {
+//     console.log(`${ signal }...`);
+//     if (err) console.error(err.stack || err);
+//     setTimeout(() => {
+//       console.log('...waited 5s, exiting.');
+//       process.exit(err ? 1 : 0);
+//     }, 5000).unref();
+//   };
+// }
 
     //end
 
